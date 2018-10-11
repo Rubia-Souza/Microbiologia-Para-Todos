b9f344b85858e32a363c7a53bd134fe2025ec0f5
Rubens Souza <41390323+Rubens-Souza@users.noreply.github.com>
2018-10-11 08:25:03 -0300

    Update perguntas-questionario-script.js

--- scripts/perguntas-questionario-script.js
+++ scripts/perguntas-questionario-script.js
@@ -53,126 +53,206 @@ let questionario1 = [
 ];
 
 let questionario2 = [
-  "",
+  "CCBBBDACAA",
 
   [
-    "pergunta1",
-    "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(USU-RJ)</strong> Na mucosa intestinal, as células apresentam  grande capacidade de absorção devido à presença de: ",
+    "desmossomas", "vesículas fagocitárias", "microvilosidades", "flagelos"
   ],
 
   [
-    "pergunta2",
-    "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(MOJI-SP)</strong> A membrana plasmática, apesar de  invisível ao microscópio óptico, está presente:",
+    "apenas nas células animais.", "apenas nas células dos procariontes.", "em todas as células, seja ela procariótica ou eucariótica.", "apenas nas células dos eucariontes."
   ],
 
   [
-    "pergunta3",
-    "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(UF-AC)</strong> Quimicamente, a membrana celular é constituída principalmente por:",
+    "acetonas e ácidos graxos.", "proteínas e lipídios.", "carboidratos e ácidos nucleicos.", "RNA e DNA."
   ],
 
   [
-    "pergunta4",
-    "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(VEST-RIO-92)</strong> Os seres vivos, exceto os vírus, apresentam estrutura celular. Entretanto, não há nada que corresponda a uma célula típica, pois, tanto os organismos unicelulares como as células dos vários tecidos dos pluricelulares são muito diferentes entre si. Apesar dessa enorme variedade, todas as células vivas apresentam o seguinte componente:",
+    "retículo endoplasmático.", "membrana plasmática.", "aparelho de Golgi.", "mitocôndria."
   ],
 
   [
-    "pergunta5",
-    "resposta1", "resposta2", "resposta3", "resposta4"
-  ]
+    "<strong>(PUC-RJ)</strong> As células animais diferem das células vegetais porque estas contêm várias estruturas e organelas características. Na lista abaixo, marque a organela ou estrutura comum às células animais e vegetais.",
+    "vacúolo", "membrana celular", "parede celular", "cloroplastos"
+  ],
+
+  [
+    "<strong>(UFF-95)</strong> Assinale, dentre as estruturas abaixo, aquela que representa EXCEÇÃO à especialização da membrana plasmática:",
+    "microvilosidades intestinais.", "pêlos absorventes na raízes dos vegetais.", "axônio.", "cílios."
+  ],
+
+  [
+    "<strong>(PUC-SP)</strong> As microvilosidades presentes nas células do epitélio intestinal têm a função de:",
+    "aumentar a superfície de absorção.", "produzir grande quantidade de ATP, necessária ao intenso metabolismo celular.", "sintetizar enzimas digestivas.", "aumentar a aderência entre uma célula e outra."
+  ],
+
+  [
+    "O reforço externo da membrana celular nos vegetais é:",
+    "rígido, celulósico e colado à membrana plasmática.", "elástico, celulósico e colado à membrana plasmática.", "rígido, celulósico e capaz de se descolar da membrana plasmática.", "elástico, celulósico e capaz de se destacar da membrana plasmática."
+  ],
+
+  [
+    "Numa célula animal, qual organela é responsável pela respiração celular?",
+    "Mitocôndria", "Ribossomo", "Centríolo", "Complexo de Golgi"
+  ],
+
+  [
+    "Qual das alternativas abaixo apresenta funções da membrana plasmática numa célula?",
+    "Dar forma à célula, além de controlar as substâncias que entram e saem dela.", "Executar a respiração celular.", "Fazer a síntese de proteínas dentro da célula.", "Executar todo processo de divisão celular."
+  ],
 ];
 
 let questionario3 = [
-  "",
+  "CDABD",
 
   [
-    "pergunta1", "resposta1", "resposta2", "resposta3", "resposta4"
+    "O reino Monera é composto dos seres mais abundantes do planeta, também conhecidos como microrganismos, pois todos são unicelulares e microscópicos. Apresentam também a ausência da carioteca, sendo, portanto, classificadas como procariotos. Os grupos que compõem o reino Monera são",
+    "Protozoários e Bactérias", "Algas e Bactérias", "Arqueas e Bactérias", "Levedos e Bactérias"
   ],
 
   [
-    "pergunta2", "resposta1", "resposta2", "resposta3", "resposta4"
+    "As bactérias apresentam como mecanismo para promoção da mistura de genes entre indivíduos diferentes, o processo de recombinação genética. Este processo pode ocorrer de três formas, sendo que uma delas é através da formação de uma ponte (pili) entre as duas células, ocorrendo a migração de genes de uma bactéria para outra. Essa forma de recombinação é conhecida como:",
+    "Transformação bacteriana", "Transdução bacteriana", "Divisão bacteriana", "Conjugação bacteriana"
   ],
 
   [
-    "pergunta3", "resposta1", "resposta2", "resposta3", "resposta4"
+    "A utilização de microrganismos na limpeza de áreas ambientais contaminadas por poluentes é uma estratégia simples, menos onerosa, além de causar menos impacto ao meio ambiente. Esse tipo de ação é feita com o uso de bactérias e é conhecido como:",
+    "Biorremediação", "Antibiótico", "Arqueas", "Vacina ambiental"
   ],
 
   [
-    "pergunta4", "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(UFMG)</strong> Em que alternativa as duas características são comuns a todos os indivíduos do reino Monera?",
+    "Ausência de núcleo e presença de clorofila", "Ausência de carioteca e capacidade de síntese proteica", "Incapacidade de síntese proteica e parasitas exclusivos", "Presença de um só tipo de ácido nucleico e ausência de clorofila"
   ],
 
   [
-    "pergunta5", "resposta1", "resposta2", "resposta3", "resposta4"
+    "Marque a alternativa que indica uma estrutura ausente em todas as bactérias:",
+    "Flagelos", "Membrana plasmática", "Parede celular", "Carioteca"
   ]
 ];
 
 let questionario4 = [
-  "",
+  "ACCABBA",
 
   [
-    "pergunta1", "resposta1", "resposta2", "resposta3", "resposta4"
+    "No Reino Fungi, é possível encontrar representantes multicelulares e unicelulares. Entre os organismos indicados a seguir, marque a alternativa que apresenta um exemplo de fungo unicelular.",
+    "Levedura", "Bolor", "Orelha-de-pau", "Cogumelo"
   ],
 
   [
-    "pergunta2", "resposta1", "resposta2", "resposta3", "resposta4"
+    "Os fungos apresentam grande aplicabilidade econômica e também ecológica. Entretanto, alguns provocam doenças no organismo.<br>Assinale a alternativa que não apresenta uma doença causada por fungo:",
+    "Pé-de-atleta", "Criptococose", "Erisipela", "Candidíase"
   ],
 
   [
-    "pergunta3", "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(Cesgranrio-RJ)</strong> Assinale a opção que apresenta uma característica ausente no reino Fungi.",
+    "Reprodução assexuada", "Respiração anaeróbia", "Célula procariótica", "Relação mutualística"
   ],
 
   [
-    "pergunta4", "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(Cesumar-PR)</strong> Os fungos são seres representados pelos cogumelos, leveduras, orelhas-de-pau e vários tipos de bolores. São importantes na cadeia alimentar, pois constituem um dos agentes decompositores de matéria orgânica, sendo úteis também na alimentação humana, na indústria e na medicina. Sobre esses seres, podemos dizer corretamente que:",
+    "são heterótrofos e aclorofilados.", "são na maioria autótrofos e responsáveis pela devolução de matéria inorgânica ao ambiente.", "possuem amido como substância de reserva e todos são microscópicos.", "muitas espécies são causadoras de doenças como a malária, o tétano e a tuberculose."
   ],
 
   [
-    "pergunta5", "resposta1", "resposta2", "resposta3", "resposta4"
+    "As algas multicelulares são organismos fotossintetizantes que por muito tempo foram consideradas plantas. Entretanto, com o avanço da ciência, percebeu-se que esses organismos não possuíam tecidos e órgãos especializados. A partir daí, essas algas passaram a ser agrupadas no Reino:",
+    "Monera", "Protoctista", "Fungi", "Animalia"
+  ],
+
+  [
+    "Sobre os representantes do Reino Protoctista, marque a alternativa incorreta:",
+    "O Reino Protoctista engloba seres uni e multicelulares.", "No Reino Protoctista encontramos apenas algas unicelulares e protozoários.", "O Reino Protoctista possui representantes heterotróficos.", "No Reino Protoctista encontramos algas e protozoários."
+  ],
+
+  [
+    "<strong>(UFMA)</strong> Assinale a opção em que todas as doenças são causadas por protozoários.",
+    "malária, doença de Chagas, leishmaniose, amebíase.", "malária, doença de Chagas, peste bubônica, amebíase.", "malária, febre amarela, doença de Chagas e amebíase.", "peste bubônica, doença de Chagas, febre amarela, amebíase."
   ]
 ];
 
 let questionario5 = [
-  "",
+  "ACDCCCDBBA",
 
   [
-    "pergunta1", "resposta1", "resposta2", "resposta3", "resposta4"
+    "Marque a alternativa em que todas as doenças citadas são virais:",
+    "Herpes, condiloma acuminado, hepatite e mononucleose", "AIDS, dengue, malária, gripe", "Catapora, rubéola, caxumba, tétano", "Leishmaniose, AIDS, sífilis e tétano."
   ],
 
   [
-    "pergunta2", "resposta1", "resposta2", "resposta3", "resposta4"
+    "O HIV é um exemplo de vírus envelopado. Marque a alternativa que explica corretamente essa definição.",
+    "São vírus envolvidos por um envelope proteico.", "São vírus envolvidos por uma parede celular.", "São vírus envolvidos por um envelope lipoproteico.", "São vírus com um envelope quitinoso."
   ],
 
   [
-    "pergunta3", "resposta1", "resposta2", "resposta3", "resposta4"
+    "Como se denomina o tipo de vírus que ataca bactérias?",
+    "Intracelular obrigatório", "Bactericida", "Bacteriose", "Bacteriófago"
   ],
 
   [
-    "pergunta4", "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(UFRN)</strong> Todos os vírus são constituídos por:",
+    "DNA e proteínas", "Aminoácidos e água", "Ácidos nucléicos e proteínas", "DNA e RNA."
   ],
 
   [
-    "pergunta5", "resposta1", "resposta2", "resposta3", "resposta4"
-  ]
+    "<strong>(FATEC-SP)</strong> Os vírus são minúsculos \"piratas\" biológicos porque invadem as células, saqueiam seus nutrientes e utilizam as reações químicas das mesmas para se reproduzir. Logo em seguida os descendentes dos invasores transmitem-se a outras células, provocando danos devastadores. A estes danos, dá-se o nome de virose, como a raiva, a dengue hemorrágica, o sarampo, a gripe, etc. (Texto modificado do livro \"PIRATAS DA CÉLULA\", de Andrew Scott.) <br>De acordo com o texto, é correto afirmar:",
+    "Os vírus utilizam o seu próprio metabolismo para destruir células, causando viroses.", "Os vírus utilizam o DNA da célula hospedeira para produzir outros vírus.", "Os vírus não têm metabolismo próprio.", "As viroses resultam sempre das modificações genéticas da célula hospedeira."
+  ],
+
+  [
+    "Os vírus não são considerados seres vivos por muitos autores, uma vez que não são capazes de realizar atividades metabólicas fora de uma célula. Por essa razão, eles são chamados de:",
+    "bacteriófagos", "organismos metabolicamente inativos", "parasitas intracelulares obrigatórios", "inquilinos intercelulares."
+  ],
+
+  [
+    "Os vírus são formados basicamente por moléculas de ácido nucleico, que pode ser DNA ou RNA, envoltas por proteínas. A capa de proteína que envolve o ácido nucleico é chamada de:",
+    "Nucleocapsídio", "Envelope viral", "Interferon", "Capsídeo."
+  ],
+
+  [
+    "Cada vírus só é capaz de parasitar células específicas, uma vez que em sua superfície são encontradas proteínas que se encaixam perfeitamente em receptores localizados na membrana das células hospedeiras. As proteínas encontradas na superfície do capsídeo ou do envelope lipoproteico recebem o nome de:",
+    "conectoras", "ligantes", "carregadoras", "virulentas."
+  ],
+
+  [
+    "<strong>(UEMS)</strong> Sobre os vírus, é correta a afirmação:",
+    "Todos os vírus têm DNA na sua constituição.", "Os vírus diferem dos seres vivos por serem acelulares", "Não necessitam de outros organismos para sua reprodução", "Não infectam células bacterianas."
+  ],
+
+  [
+    "<strong>(MACKENZIE-SP)</strong> O ser humano tem travado batalhas constantes contra os vírus. A mais recente é contra o vírus H1N1, que causa a “gripe suína”. A respeito dos vírus, assinale a alternativa correta.",
+    "São todos endoparasitas celulares.", "São todos endoparasitas celulares.", "Todos eles possuem o DNA e o RNA como material genético.", "Todos eles possuem o DNA e o RNA como material genético."
+  ],
 ];
 
 let questionario6 = [
-  "",
+  "DBCAB",
 
   [
-    "pergunta1", "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(Enem 2009)</strong> Um novo método para produzir insulina artificial que utiliza tecnologia de DNA recombinante foi desenvolvido por pesquisadores do Departamento de Biologia Celular da Universidade de Brasília (UnB) em parceria com a iniciativa privada. Os pesquisadores modificaram geneticamente a bactéria Escherichia coli para torná-la capaz de sintetizar o hormônio. O processo permitiu fabricar insulina em maior quantidade e em apenas 30 dias, um terço do tempo necessário para obtê-la pelo método tradicional, que consiste na extração do hormônio a partir do pâncreas de animais abatidos. <a herf=\"http://cienciahoje.uol.com.br\">Ciência Hoje, 24 abr. 2001.</a><br>A produção de insulina pela técnica do DNA recombinante tem, como consequência,",
+    "o aperfeiçoamento do processo de extração de insulina a partir do pâncreas suíno.", "a seleção de microrganismos resistentes a antibióticos.", "o progresso na técnica da síntese química de hormônios.", "impacto favorável na saúde de indivíduos diabéticos."
   ],
 
   [
-    "pergunta2", "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(Enem 2012)</strong> O milho transgênico é produzido a partir da manipulação do milho original, com a transferência, para este, de um gene de interesse retirado de outro organismo de espécie diferente.<br>A característica de interesse será manifestada em decorrência",
+    "do incremento do DNA a partir da duplicação do gene transferido.", "da tradução do RNA mensageiro sintetizado a partir do DNA recombinante.", "da transcrição do RNA transportador a partir do gene transferido.", "da síntese de carboidratos a partir da ativação do DNA do milho original."
   ],
 
   [
-    "pergunta3", "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(Enem 2013)</strong> A estratégia de obtenção de plantas transgênicas pela inserção de transgenes em cloroplastos, em substituição à metodologia clássica de inserção do transgene no núcleo da célula hospedeira, resultou no aumento quantitativo da produção de proteínas recombinantes com diversas finalidades biotecnológicas. O mesmo tipo de estratégia poderia ser utilizada para produzir proteínas recombinantes em células de organismos eucarióticos não fotossintetizantes, como as leveduras, que são usadas para produção comercial de várias proteínas recombinantes e que podem ser cultivadas em grandes fermentadores.<br>Considerando a estratégia metodológica descrita, qual organela celular poderia ser utilizada para inserção de transgenes em leveduras?",
+    "Lisossomo", "Peroxissomo", "Mitocôndria", "Complexo golgiense"
   ],
 
   [
-    "pergunta4", "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(Enem 2012)</strong> Há milhares de anos o homem faz uso da biotecnologia para a produção de alimentos como pães, cervejas e vinhos. Na fabricação de pães, por exemplo, são usados fungos unicelulares, chamados de leveduras, que são comercializados como fermento biológico. Eles são usados para promover o crescimento da massa, deixando-a leve e macia. O crescimento da massa do pão pelo processo citado é resultante da",
+    "liberação de gás carbônico", "formação de água", "formação de ácido lático", "produção de ATP"
   ],
 
   [
-    "pergunta5", "resposta1", "resposta2", "resposta3", "resposta4"
+    "<strong>(Enem 2012)</strong> Pesticidas são contaminantes ambientais altamente tóxicos aos seres vivos e, geralmente, com grande persistência ambiental. A busca por novas formas de eliminação dos pesticidas tem aumentado nos últimos anos, uma vez que as técnicas atuais são economicamente dispendiosas e paliativas. A biorremediação de pesticidas utilizando microrganismos tem se mostrado uma técnica muito promissora para essa finalidade, por apresentar vantagens econômicas e ambientais.<br>Para ser utilizado nesta técnica promissora, um microrganismo deve ser capaz de",
+    "transferir o contaminante do solo para a água", "metabolizar o contaminante, liberando subprodutos menos tóxicos ou atóxicos", "absorver o contaminante sem alterá-lo quimicamente", "estimular o sistema imunológico do homem contra o contaminante"
   ]
 ];
